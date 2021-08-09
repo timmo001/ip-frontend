b93e31322eeb6ca3b5a4f4e009a1c4329799c45a
Aidan Timson <contact@timmo.xyz>
2021-08-10 00:36:50 +0100

    eslint ignore

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = withCSS(
   withFonts({
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
     webpack: (config) => {
       config.module.rules.push({
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
