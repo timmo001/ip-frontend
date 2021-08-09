b2b44cec335f7b1e7aa261634640252ebe91b7a2
Aidan Timson <contact@timmo.xyz>
2021-08-09 21:04:29 +0100

    Scripts

---
+++ scripts/package.js
@@ -0,0 +1,41 @@
+const { copyFileSync, existsSync, mkdirSync } = require("fs");
+const { exec } = require("pkg");
+const { join } = require("path");
+
+const filePaths = [
+  {
+    from: "../LICENSE",
+    to: "../out/LICENSE",
+  },
+];
+
+async function package() {
+  await exec([
+    join(__dirname, "../"),
+    "--output",
+    join(
+      __dirname,
+      `../out/ip-frontend${process.platform === "win32" ? ".exe" : ""}`
+    ),
+  ]);
+
+  filePaths
+    .filter((path) =>
+      path.platform ? path.platform === process.platform : true
+    )
+    .forEach((path) => {
+      const sourceFile = join(__dirname, path.from);
+      if (existsSync(sourceFile)) {
+        const targetDir = join(
+          __dirname,
+          path.to.substring(0, path.to.lastIndexOf("/"))
+        );
+        if (!existsSync(targetDir)) mkdirSync(targetDir);
+        const targetFile = join(__dirname, path.to);
+        console.log(`Copy ${sourceFile} to ${targetFile}`);
+        copyFileSync(sourceFile, targetFile);
+      }
+    });
+}
+
+package();
